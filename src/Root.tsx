ab81e7d1efd3b7f3d7c30dbc6aee375271f37c0b
Tew Tawan <hello@tawan.org>
2021-05-05 06:13:33 +0000

    chore: html

--- src/Root.tsx
+++ src/Root.tsx
@@ -10,12 +10,14 @@ import HomePage from "./containers/HomePage";
 import NoMatch from "./components/NoMatch";
 
 const AppContainer: React.FC = () => (
+  <>
   <Router>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="*" component={NoMatch} />
     </Switch>
   </Router>
+  </>
 )
 
 export default AppContainer;
