713a854c9645653a800d7533cfb4f6548de1fdd7
Tawan <10970130+tawanorg@users.noreply.github.com>
2021-05-07 15:31:06 +0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -15,7 +15,7 @@ export const APP_SUBTITLE = 'Everything you’ve ever wanted to know about me an
 export const SKILLS: ISkill[] = [
   {
     name: 'TypeScript',
-    description: 'Writing Type-Safe(ish) JS Code',
+    description: 'Writing Type-Safe(ish) apps',
     iconComponent: TSIcon,
     iconSize: 14,
   },
